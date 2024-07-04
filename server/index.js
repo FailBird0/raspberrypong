54e594a8e30c7bbf810ac887ac384e88efae14c3
FailBird0 <desdonoc@gmail.com>
2024-07-04 13:31:54 +0200

    oopsie #2

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ try {
   oledDisplay.turnOnDisplay();
   oledDisplay.clearDisplay();
   oledDisplay.dimDisplay(false);
-  oledDisplay.writeString(oledFont, 1, "Pong Server Online", 1, true);
+  oledDisplay.writeString(oledFont, 1, "  Pong Server Online\n", 1, true);
 
   let ipsString = "";
 
